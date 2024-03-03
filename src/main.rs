f715366cfe4f93bb9ec2ffd2d029f8a26f31283a
szymros <szymon.roslaniecc@gmail.com>
2024-03-03 21:55:36 +0100

    add instancing

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,15 @@
+use cgmath::prelude::*;
 use wgpu::util::DeviceExt;
 use winit::event_loop;
 
 const window_size: (u32, u32) = (800, 600);
+
+const NUM_INSTANCES_PER_ROW: u32 = 10;
+const INSTANCE_DISPLACEMENT: cgmath::Vector3<f32> = cgmath::Vector3::new(
+    NUM_INSTANCES_PER_ROW as f32 * 0.5,
+    0.0,
+    NUM_INSTANCES_PER_ROW as f32 * 0.5,
+);
 
 #[repr(C)]
 #[derive(Copy, Clone, Debug, bytemuck::Pod, bytemuck::Zeroable)]
@@ -12,7 +20,15 @@ struct Vertex {
 
 impl Vertex {
     fn new(position: [f32; 3], color: [f32; 3]) -> Self {
-        Vertex { position:[position[0] as f32, position[1] as f32, position[2] as f32, 1.0], color }
+        Vertex {
+            position: [
+                position[0] as f32,
+                position[1] as f32,
+                position[2] as f32,
+                1.0,
+            ],
+            color,
+        }
     }
     fn desc() -> wgpu::VertexBufferLayout<'static> {
         wgpu::VertexBufferLayout {
@@ -79,17 +95,74 @@ const INDEX: &[u16] = &[
     20, 21, 22, 22, 23, 20, // back
 ];
 
+#[repr(C)]
+#[derive(Copy, Clone, Debug, bytemuck::Pod, bytemuck::Zeroable)]
+struct InstanceRaw {
+    model: [[f32; 4]; 4],
+}
+
+impl InstanceRaw {
+    fn desc() -> wgpu::VertexBufferLayout<'static> {
+        use std::mem;
+        wgpu::VertexBufferLayout {
+            array_stride: mem::size_of::<InstanceRaw>() as wgpu::BufferAddress,
+            step_mode: wgpu::VertexStepMode::Instance,
+            attributes: &[
+                wgpu::VertexAttribute {
+                    offset: 0,
+                    // While our vertex shader only uses locations 0, and 1 now, in later tutorials, we'll
+                    // be using 2, 3, and 4, for Vertex. We'll start at slot 5, not conflict with them later
+                    shader_location: 5,
+                    format: wgpu::VertexFormat::Float32x4,
+                },
+                wgpu::VertexAttribute {
+                    offset: mem::size_of::<[f32; 4]>() as wgpu::BufferAddress,
+                    shader_location: 6,
+                    format: wgpu::VertexFormat::Float32x4,
+                },
+                wgpu::VertexAttribute {
+                    offset: mem::size_of::<[f32; 8]>() as wgpu::BufferAddress,
+                    shader_location: 7,
+                    format: wgpu::VertexFormat::Float32x4,
+                },
+                wgpu::VertexAttribute {
+                    offset: mem::size_of::<[f32; 12]>() as wgpu::BufferAddress,
+                    shader_location: 8,
+                    format: wgpu::VertexFormat::Float32x4,
+                },
+            ],
+        }
+    }
+}
+
+struct Instance {
+    position: cgmath::Vector3<f32>,
+    rotation: cgmath::Quaternion<f32>,
+}
+
+impl Instance {
+    fn to_raw(&self) -> InstanceRaw {
+        InstanceRaw {
+            model: (cgmath::Matrix4::from_translation(self.position)
+                * cgmath::Matrix4::from(self.rotation))
+            .into(),
+        }
+    }
+}
+
 const OPENGL_TO_WGPU_MATRIX: cgmath::Matrix4<f32> = cgmath::Matrix4::new(
-    1.0, 0.0, 0.0, 0.0,
-    0.0, 1.0, 0.0, 0.0,
-    0.0, 0.0, 0.5, 0.5,
-    0.0, 0.0, 0.0, 1.0,
+    1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.5, 0.5, 0.0, 0.0, 0.0, 1.0,
 );
 
-fn generate_view_matrix()-> cgmath::Matrix4<f32>{
-    let projection = cgmath::perspective(cgmath::Deg(45.0), window_size.0 as f32 / window_size.1 as f32, 1.0, 10.0);
+fn generate_view_matrix() -> cgmath::Matrix4<f32> {
+    let projection = cgmath::perspective(
+        cgmath::Deg(45.0),
+        window_size.0 as f32 / window_size.1 as f32,
+        1.0,
+        10.0,
+    );
     let view = cgmath::Matrix4::look_at_rh(
-        cgmath::Point3::new(1.5, -5.0, 3.0),
+        cgmath::Point3::new(0.0, -5.0, 3.0),
         cgmath::Point3::new(0.0, 0.0, 0.0),
         cgmath::Vector3::unit_y(),
     );
@@ -156,7 +229,36 @@ async fn init_gpu(window: &winit::window::Window) {
         usage: wgpu::BufferUsages::INDEX,
     });
 
-    let view_matrix: [[f32;4];4] = generate_view_matrix().into();
+    let instances = (0..NUM_INSTANCES_PER_ROW)
+        .flat_map(|z| {
+            (0..NUM_INSTANCES_PER_ROW).map(move |x| {
+                let position = cgmath::Vector3 {
+                    x: x as f32,
+                    y: 0.0,
+                    z: z as f32,
+                } - INSTANCE_DISPLACEMENT;
+
+                let rotation = if position.is_zero() {
+                    // this is needed so an object at (0, 0, 0) won't get scaled to zero
+                    // as Quaternions can affect scale if they're not created correctly
+                    cgmath::Quaternion::from_axis_angle(cgmath::Vector3::unit_z(), cgmath::Deg(0.0))
+                } else {
+                    cgmath::Quaternion::from_axis_angle(position.normalize(), cgmath::Deg(45.0))
+                };
+
+                Instance { position, rotation }
+            })
+        })
+        .collect::<Vec<_>>();
+
+    let instance_data = instances.iter().map(Instance::to_raw).collect::<Vec<_>>();
+    let instance_buffer = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
+        label: Some("Instance Buffer"),
+        contents: bytemuck::cast_slice(&instance_data),
+        usage: wgpu::BufferUsages::VERTEX,
+    });
+
+    let view_matrix: [[f32; 4]; 4] = generate_view_matrix().into();
 
     let camera_uniform_buffer = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
         label: Some("Uniform Buffer"),
@@ -165,30 +267,27 @@ async fn init_gpu(window: &winit::window::Window) {
     });
 
     let camera_bind_group_layout =
-    device.create_bind_group_layout(&wgpu::BindGroupLayoutDescriptor {
-        entries: &[wgpu::BindGroupLayoutEntry {
-            binding: 0,
-            visibility: wgpu::ShaderStages::VERTEX,
-            ty: wgpu::BindingType::Buffer {
-                ty: wgpu::BufferBindingType::Uniform,
-                has_dynamic_offset: false,
-                min_binding_size: None,
-            },
-            count: None,
-        }],
-        label: Some("camera_bind_group_layout"),
-    });
-
+        device.create_bind_group_layout(&wgpu::BindGroupLayoutDescriptor {
+            entries: &[wgpu::BindGroupLayoutEntry {
+                binding: 0,
+                visibility: wgpu::ShaderStages::VERTEX,
+                ty: wgpu::BindingType::Buffer {
+                    ty: wgpu::BufferBindingType::Uniform,
+                    has_dynamic_offset: false,
+                    min_binding_size: None,
+                },
+                count: None,
+            }],
+            label: Some("camera_bind_group_layout"),
+        });
 
     let camera_bind_group = device.create_bind_group(&wgpu::BindGroupDescriptor {
         label: Some("Camera Bind Group"),
         layout: &camera_bind_group_layout,
-        entries: &[
-            wgpu::BindGroupEntry {
-                binding: 0,
-                resource: camera_uniform_buffer.as_entire_binding(),
-            },
-        ],
+        entries: &[wgpu::BindGroupEntry {
+            binding: 0,
+            resource: camera_uniform_buffer.as_entire_binding(),
+        }],
     });
 
     let pipeline_layout = device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
@@ -203,7 +302,7 @@ async fn init_gpu(window: &winit::window::Window) {
         vertex: wgpu::VertexState {
             module: &shader_module,
             entry_point: "vs_main",
-            buffers: &[Vertex::desc()],
+            buffers: &[Vertex::desc(), InstanceRaw::desc()],
         },
         primitive: wgpu::PrimitiveState {
             topology: wgpu::PrimitiveTopology::TriangleList,
@@ -234,7 +333,9 @@ async fn init_gpu(window: &winit::window::Window) {
     });
 
     let output = surface.get_current_texture().unwrap();
-    let view = output.texture.create_view(&wgpu::TextureViewDescriptor::default());
+    let view = output
+        .texture
+        .create_view(&wgpu::TextureViewDescriptor::default());
     let mut encoder = device.create_command_encoder(&wgpu::CommandEncoderDescriptor {
         label: Some("Render Encoder"),
     });
@@ -246,7 +347,7 @@ async fn init_gpu(window: &winit::window::Window) {
                 resolve_target: None,
                 ops: wgpu::Operations {
                     load: wgpu::LoadOp::Clear(wgpu::Color::BLACK),
-                    store: wgpu::StoreOp::Store
+                    store: wgpu::StoreOp::Store,
                 },
             })],
             depth_stencil_attachment: None,
@@ -257,14 +358,14 @@ async fn init_gpu(window: &winit::window::Window) {
         render_pass.set_bind_group(0, &camera_bind_group, &[]);
         render_pass.set_index_buffer(index_buffer.slice(..), wgpu::IndexFormat::Uint16);
         render_pass.set_vertex_buffer(0, vertex_buffer.slice(..));
-        render_pass.draw_indexed(0..INDEX.len() as u32, 0, 0..1);
+        render_pass.set_vertex_buffer(1, instance_buffer.slice(..));
+        render_pass.draw_indexed(0..INDEX.len() as u32, 0, 0..instances.len() as _);
     }
     queue.submit(std::iter::once(encoder.finish()));
     output.present();
-
 }
 
-async fn run(){
+async fn run() {
     let event_loop = event_loop::EventLoop::new().unwrap();
     let window = winit::window::WindowBuilder::new()
         .build(&event_loop)
@@ -277,13 +378,12 @@ async fn run(){
             event: winit::event::WindowEvent::CloseRequested,
             ..
         } => window_target.exit(),
-    winit::event::Event::WindowEvent {event: winit::event::WindowEvent::RedrawRequested,
-        .. } => {
-    }
+        winit::event::Event::WindowEvent {
+            event: winit::event::WindowEvent::RedrawRequested,
+            ..
+        } => {}
         _ => (),
-    }
-);
-
+    });
 }
 
 fn main() {
--- src/shader.wgsl
+++ src/shader.wgsl
@@ -10,13 +10,31 @@ struct VertexOutput {
     @location(0) color: vec3<f32>,
 };
 
+struct VertexInput {
+    @location(0) position: vec4<f32>,
+    @location(1) color: vec3<f32>,
+};
+
+struct InstanceInput {
+    @location(5) model_matrix_0: vec4<f32>,
+    @location(6) model_matrix_1: vec4<f32>,
+    @location(7) model_matrix_2: vec4<f32>,
+    @location(8) model_matrix_3: vec4<f32>,
+};
+
 @vertex
 fn vs_main(
-    @location(0) position: vec4<f32>, @location(1) color: vec3<f32>
+     vertex: VertexInput,instance: InstanceInput
 ) -> VertexOutput {
+    let model_matrix = mat4x4<f32>(
+        instance.model_matrix_0,
+        instance.model_matrix_1,
+        instance.model_matrix_2,
+        instance.model_matrix_3,
+    );
     var out: VertexOutput;
-    out.clip_position = view_matrix * position;
-    out.color = color;
+    out.clip_position = view_matrix * model_matrix * vertex.position;
+    out.color = vertex.color;
     return out;
 }
 
